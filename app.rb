d971c520c1bf6512ad72ec9385458a8c4a4d2b41
Bobby Neal <rsneal01@gmail.com>
2021-01-03 18:24:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     erb :user_input
   end
   
-  post '/piglatinize' do
+  post '/' do
     user_text = params[:word]
     @latinized_word = PigLatinizer.new(user_text)
     
