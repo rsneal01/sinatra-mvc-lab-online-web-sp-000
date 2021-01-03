2906bc6dfd28ba51185ef3a8890cad22a450962b
Bobby Neal <rsneal01@gmail.com>
2021-01-03 18:19:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :user_input
+  end
+  
+  post '/piglatinize' do
+    user_text = params[:word]
+    @latinized_word = PigLatinizer.new(user_text)
+    
+    erb :new_string
+  end
+  
+  
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,34 @@
+class PigLatinizer
+  attr_reader :text
+  
+  def initialize
+    @text = text  
+  end
+  
+  def piglatinize(string)
+    a = string.split(" ")
+    b = a.map {|word| pig_latinize(word)}
+    b.join(" ")
+  end
+  
+  def pig_latinize(word)
+    
+    first_letter = word[0].downcase
+    if ["a", "e", "i", "o", "u"].include?(first_letter)
+        "#{word}way"
+        # piglatinize word that starts with a vowel
+    else
+      consonants = []
+      consonants << word[0]
+        if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+          consonants << word[1]
+          if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+            consonants << word[2]
+          end
+        end
+      "#{word[consonants.length..-1] + consonants.join + "ay"}"
+        # piglatinize word that starts with a consonant
+    end
+  end
+  
+end
